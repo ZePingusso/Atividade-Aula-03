49f92cab5ca5982c3d3aeb4f6ff7b2002ad86000
ZePingusso <lucasscruz622@gmail.com>
2026-08-12 22:56:33 -0400

    exercício 2 feito

--- capuccino/src/App.jsx
+++ capuccino/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     async function buscarPosts(){
-      const resposta = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const resposta = await fetch("https://jsonplaceholder.typicode.com/comments?postId=1");
       const dados = await resposta.json();
       setPosts(dados);
     }
@@ -16,7 +16,7 @@ function App() {
     <>
       <section id="center">
         {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id}>{post.name} {post.email}</li>
         ))}
       </section>
     </>
